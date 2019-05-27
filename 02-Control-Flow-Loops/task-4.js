4bb0a905f93432b2e5303572ae38eb98105c52f0
Giorgi <gigola9@yahoo.com>
2019-05-27 21:41:05 +0400

    fix(Fix task-4):

--- 02-Control-Flow-Loops/task-4.js
+++ 02-Control-Flow-Loops/task-4.js
@@ -1,12 +1,15 @@
+var a=0;
+
 switch (a) {
     case 0:
         console.log( 0 );
-    break;
+        break;
     case 1:
         console.log( 1 );
-    break;
+        break;
     case 2:
         console.log( '2,3' );
+        break
     case 3:
         console.log( '2,3' );
         break;
