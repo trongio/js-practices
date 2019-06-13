ebb9b266a76ac24659fed12336cd835a0d56629d
trongio <giokakabadze50@gmail.com>
2019-06-13 19:28:14 +0400

    fix(15-Sets-and-Maps): Fixed bugs

--- 15-Sets-and-Maps/task-1.js
+++ 15-Sets-and-Maps/task-1.js
@@ -1,105 +1,90 @@
-
 class DB {
     
-  constructor() {
-      this._users = new Map();
-  }
+    constructor() {
+        this._users = new Map();
+    }
 
-  create(user) {
-      const { name, age, country, salary } = user;
+    _validate(str, type, arg) {
 
-      if ( typeof name !== 'string' ) {
-          throw new Error('Name is required, and must be string');
-      }
+        if ( typeof str !== type ) {
+            throw new Error(`${arg} is required and must be ${type}`);
+        }
 
-      if ( typeof age !== 'number' ) {
-          throw new Error('Age is required, and must be number');
-      }
-      
-      if ( typeof country !== 'string' ) {
-          throw new Error('Country is required, and must be string');
-      }
+        return this;
+    }
 
-      if ( typeof salary !== 'number' ) {
-          throw new Error('Salary is required, and must be number');
-      }
+    create(user) {
+        const { name, age, country, salary } = user;
 
-      const id = this.readAll()[0] && (this.readAll().reverse()[0].id + 1).toString() || '1';
-      this._users.set( id, user );
+        this._validate(name, 'string', 'Name')
+            ._validate(age, 'number', 'Age')
+            ._validate(country, 'string', 'Country')
+            ._validate(salary, 'number', 'Salary');
+        
+        const id = this.readAll()[0] && (parseInt(Array.from(this._users.keys()).reverse()[0])+1).toString() || '1';
+        this._users.set( id, user );
 
-      return id;
-  }
+        return id;
+    }
 
-  read(id) {
+    read(id) {
 
-      if (typeof id === 'undefined' || typeof id !== 'string') {
-          throw new Error('First parameter must be a string.');
-      }
+        this._validate(id, 'string', 'First parameter');
 
-      const user = this._users.get(id) || null;
-      if (user) {
-          user.id = id;
-      }
-      return user;
-  }
+        const user = this._users.get(id) || null;
+        if (user) {
+            user.id = id;
+        }
+        return user;
+    }
 
-  readAll(...args) {
-      if (args.length > 0) {
-          throw new Error('ReadAll does\'n have any parameter.');
-      }
-      return Array.from(this._users.values());
-  }
+    readAll(...args) {
+        if (args.length > 0) {
+            throw new Error('ReadAll does\'n have any parameter.');
+        }
+        return Array.from(this._users.values());
+    }
 
-  update(id, data) {
+    update(id, data) {
 
-      if (typeof id === 'undefined' || typeof id !== 'string') {
-          throw new Error('First parameter must be a string.');
-      }
+        this._validate(id, 'string', 'First parameter')
+            ._validate(data, 'object', 'Second parameter');
 
-      if (!this._users.has(id)) {
-          throw new Error('User with this id does\'n exists.');
-      }
+        if (!this._users.has(id)) {
+            throw new Error('User with this id does\'n exists.');
+        }
+        
+        const user = this._users.get(id);
+        this._users.set(id, { ...user, ...data } );
 
-      if (typeof data !== 'object') {
-          throw new Error('Second parameter must be object.');
-      }
-      
-      const user = this._users.get(id);
-      user.name = data.name || user.name; 
-      user.age = data.age || user.age; 
-      user.country = data.country || user.country; 
-      user.salary = data.salary || user.salary; 
-      this._users.set( id, user );
+        return id;
 
-      return id;
+    }
 
-  }
+    delete(id) {
 
-  delete(id) {
+        this._validate(id, 'string', 'First parameter');
 
-      if (typeof id === 'undefined' || typeof id !== 'string') {
-          throw new Error('First parameter must be a string.');
-      }
+        if (!this._users.has(id)) {
+            throw new Error('User with this id does\'n exists.');
+        }
 
-      if (!this._users.has(id)) {
-          throw new Error('User with this id does\'n exists.');
-      }
-
-      return this._users.delete(id);
-  }
+        return this._users.delete(id);
+    }
 
 }
 
 const db = new DB();
 
 const person = {
-  name: 'Pitter', // required, field with type string
-  age: 21, // required, field with type number
-  country: 'ge', // required, field with type string
-  salary: 500 // required, field with type number
+    name: 'Pitter', // required field with type string
+    age: 21, // required field with type number
+    country: 'ge', // required field with type string
+    salary: 500 // required field with type number
 };
 
 const id = db.create(person);
+
 const customer = db.read(id);
 const customers = db.readAll(); // array of users
 db.update(id, { age: 22 }); // id
--- 15-Sets-and-Maps/task-2.js
+++ 15-Sets-and-Maps/task-2.js
@@ -1,135 +1,119 @@
-
-
 class DB {
     
-  constructor() {
-      this._users = new Map();
-  }
+    constructor() {
+        this._users = new Map();
+    }
 
-  create(user) {
-      const { name, age, country, salary } = user;
+    _validate(str, type, arg) {
 
-      if ( typeof name !== 'string' ) {
-          throw new Error('Name is required and must be string');
-      }
+        if ( typeof str !== type ) {
+            throw new Error(`${arg} is required and must be ${type}`);
+        }
 
-      if ( typeof age !== 'number' ) {
-          throw new Error('Age is required and must be number');
-      }
-      
-      if ( typeof country !== 'string' ) {
-          throw new Error('Country is required and must be string');
-      }
+        return this;
 
-      if ( typeof salary !== 'number' ) {
-          throw new Error('Salary is required and must be number');
-      }
+    }
 
-      const id = this.readAll()[0] && (this.readAll().reverse()[0].id + 1).toString() || '1';
-      this._users.set( id, user );
+    create(user) {
+        const { name, age, country, salary } = user;
 
-      return id;
-  }
+        this._validate(name, 'string', 'Name')
+            ._validate(age, 'number', 'Age')
+            ._validate(country, 'string', 'Country')
+            ._validate(salary, 'string', 'Salary');
 
-  read(id) {
+        const id = this.readAll()[0] && (parseInt(Array.from(this._users.keys()).reverse()[0])+1).toString() || '1';
+        this._users.set( id, user );
 
-      if (typeof id === 'undefined' || typeof id !== 'string') {
-          throw new Error('First parameter must be a string.');
-      }
+        return id;
+    }
 
-      const user = this._users.get(id) || null;
-      if (user) {
-          user.id = id;
-      }
-      return user;
-  }
+    read(id) {
 
-  readAll(...args) {
-      if (args.length > 0) {
-          throw new Error('ReadAll does\'n have any parameter.');
-      }
-      return Array.from(this._users.values());
-  }
+        this._validate(id, 'string', 'First parameter');
 
-  update(id, data) {
+        const user = this._users.get(id) || null;
+        if (user) {
+            user.id = id;
+        }
+        return user;
+    }
 
-      if (typeof id === 'undefined' || typeof id !== 'string') {
-          throw new Error('First parameter must be a string.');
-      }
+    readAll(...args) {
+        if (args.length > 0) {
+            throw new Error('ReadAll does\'n have any parameter.');
+        }
+        return Array.from(this._users.values());
+    }
 
-      if (!this._users.has(id)) {
-          throw new Error('User with this id does\'n exists.');
-      }
+    update(id, data) {
 
-      if (typeof data !== 'object') {
-          throw new Error('Second parameter must be object.');
-      }
-      
-      const user = this._users.get(id);
-      user.name = data.name || user.name; 
-      user.age = data.age || user.age; 
-      user.country = data.country || user.country; 
-      user.salary = data.salary || user.salary; 
-      this._users.set( id, user );
+        this._validate(id, 'string', 'First parameter')
+            ._validate(data, 'object', 'Second parameter');
 
-      return id;
+        if (!this._users.has(id)) {
+            throw new Error('User with this id does\'n exists.');
+        }
+        
+        const user = this._users.get(id);
+        this._users.set(id, { ...user, ...data } );
 
-  }
+        return id;
 
-  delete(id) {
+    }
 
-      if (typeof id === 'undefined' || typeof id !== 'string') {
-          throw new Error('First parameter must be a string.');
-      }
+    delete(id) {
 
-      if (!this._users.has(id)) {
-          throw new Error('User with this id does\'n exists.');
-      }
+        this._validate(id, 'string', 'First parameter');
 
-      return this._users.delete(id);
-  }
+        if (!this._users.has(id)) {
+            throw new Error('User with this id does\'n exists.');
+        }
 
-  find(query) {
-      let users = [];
+        return this._users.delete(id);
+    }
 
-      try {
-          users = this.readAll().filter(user => {
-              return (
-                  user.country === (query.country || user.country) && 
-                  user.age >= (query.age && query.age.min || 0) &&
-                  user.age <= (query.age && query.age.max || Infinity) &&
-                  user.salary >= (query.salary && query.salary.min || 0) &&
-                  user.salary <= (query.salary && query.salary.max || Infinity)
-              );
-          });
-      } catch (error) {
-          
-      }
+    find(query) {
+        let users = [];
 
-      return users;
-  }
+        try {
+            users = this.readAll().filter(user => {
+                return (
+                    user.country === (query.country || user.country) && 
+                    user.age >= (query.age && query.age.min || 0) &&
+                    user.age <= (query.age && query.age.max || Infinity) &&
+                    user.salary >= (query.salary && query.salary.min || 0) &&
+                    user.salary <= (query.salary && query.salary.max || Infinity)
+                );
+            });
+        } catch (error) {
+            
+        }
+
+        return users;
+    }
 
 }
 
 const db = new DB();
 
 const person = {
-  name: 'Pitter', // required field with type string
-  age: 21, // required field with type number
-  country: 'ge', // required field with type string
-  salary: 500 // required field with type number
+    name: 'Pitter', // required field with type string
+    age: 21, // required field with type number
+    country: 'ge', // required field with type string
+    salary: 500 // required field with type number
 };
 
 const id = db.create(person);
 
 const query = {
-  country: 'ge',
-  age: {
-      min: 21
-  },
-  salary: {
-      min: 500,
-      max: 600
-  }
+    country: 'ge',
+    age: {
+        min: 21
+    },
+    salary: {
+        min: 500,
+        max: 600
+    }
 };
 const customers = db.find(query); // array of users
